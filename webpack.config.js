003b930a22cfbc0bbe6e5705cce21b6102470240
Chris Claxton <2445837+chrisckc@users.noreply.github.com>
2018-11-29 13:12:12 +0000

    updated webpack config, added HtmlWebpackPlugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,7 @@
 const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin')
+const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
 
 module.exports = {
   entry: './src/index.js',
@@ -17,31 +20,48 @@ module.exports = {
   output: {
     path: __dirname + '/dist',
     publicPath: '/',
-    filename: 'bundle.js',
+    filename: '[name].[hash].bundle.js',
     chunkFilename: '[name].[chunkhash].chunk.js'
   },
   optimization: {
     runtimeChunk: true,
+    namedChunks: false,
+    namedModules: false,
     moduleIds: 'hashed',
     splitChunks: { // https://webpack.js.org/plugins/split-chunks-plugin/
       chunks: "initial", // default is async, set to initial and then use async inside cacheGroups as required instead
       //maxInitialRequests: Infinity, // Default is 3, make this unlimited if using HTTP/2
       //maxAsyncRequests: Infinity, // Default is 5, make this unlimited if using HTTP/2
-      //maxSize: 10000,
+      minSize: 5000,
+      maxSize: 10000,
       cacheGroups: {
         default: false, // disable the built-in groups, default & vendors (vendors is overwritten below)
+        reactDom: {
+          test: /[\\/]node_modules[\\/]react-dom[\\/]/,
+          name: "vendor.react-dom",
+          priority: 20,
+          enforce: true,
+          //minSize: 2000,
+          //maxSize: 5000
+        },
         vendors: {
           test: /[\\/]node_modules[\\/]/,
           name: "vendors",
           priority: 10,
-          //enforce: true, // create chunk regardless of the size of the chunk
-          maxSize: 10000
+          enforce: true, // create chunk regardless of the size of the chunk
+          //maxInitialRequests: Infinity
+          //minSize: 2000
         }
       }
     }
   },
   plugins: [
-    new webpack.HotModuleReplacementPlugin()
+    new CleanWebpackPlugin([ 'dist' ]),
+    new HtmlWebpackPlugin({
+      template: 'index.html',
+    }),
+    new webpack.HotModuleReplacementPlugin(),
+    new BundleAnalyzerPlugin({ analyzerPort: 9080 })
   ],
   devServer: {
     contentBase: './dist',
